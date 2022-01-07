b850221d356ab8af43b9c89fa8aa4547a60b9512
adinarad <adinarad1999@gmail.com>
2022-01-07 13:48:15 +0530

    Add Procfile and Update ports, gitignore

--- app.js
+++ app.js
@@ -137,6 +137,6 @@ app.get('/:customListName', (req, resp) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000.");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started at port " + (process.env.PORT || 3000));
 });
